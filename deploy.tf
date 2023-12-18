7601d1dc720d3b5ad496cf4a3a21bd2c09d5a581
shekarv0204 <154243101+shekarv0204@users.noreply.github.com>
2023-12-18 15:19:30 +0000

    Update deploy.tf

--- deploy.tf
+++ deploy.tf
@@ -1,48 +1,41 @@
 provider "aws" {
   region = "eu-central-1"
+  access_key = "AKIAWAKSONSC2XHE5NO2"
+  secret_key = "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"
+
 }
 
 data "archive_file" "lambda_code" {
   type        = "zip"
-  source_dir  = "weatherdata/script"
+  source_dir  = "./"
   output_path = "weatherdata/script.zip"
+  excludes    = [".terraform/", ".terraform.lock.hcl", "terraform.tfstate"]
 }
 resource "aws_s3_bucket" "lambda_code_bucket" {
-  bucket = "your-lambda-code-bucket-name"
-  acl    = "private"
+  bucket = "weather-data-shekar"
 }
 
-resource "aws_s3_bucket_object" "lambda_code_object" {
+resource "aws_s3_object" "lambda_code_object" {
   bucket = aws_s3_bucket.lambda_code_bucket.bucket
-  key    = "lambda_code.zip"
+  key    = "script.zip"
   source = data.archive_file.lambda_code.output_path
-  acl    = "private"
 }
 
 resource "aws_lambda_function" "api_aggregation_lambda" {
   function_name = "api_aggregation_lambda"
-  handler      = "lambda_function.lambda_handler"
-  runtime      = "python3.8"
-  timeout      = 60
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  timeout       = 90
 
-  filename      = "weatherdata/script.zip"
   source_code_hash = filebase64(data.archive_file.lambda_code.output_path)
   # Specify the S3 bucket and object storing the deployment
   s3_bucket = aws_s3_bucket.lambda_code_bucket.bucket
-  s3_key    = aws_s3_bucket_object.lambda_code_object.key
-
+  s3_key    = aws_s3_object.lambda_code_object.key
   role = aws_iam_role.lambda_execution_role.arn
-
-  environment {
-    variables = {
-      API_URL = "https://api.open-meteo.com/v1/forecast?latitude=51.5085&amp;longitude=-0.1257&amp;hourly=temperature_2m,rain,showers,visibility&amp;past_days=31"
-    }
-  }
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
   name = "lambda_execution_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -61,6 +54,5 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.api_aggregation_lambda.function_name
-
   principal = "apigateway.amazonaws.com"
 }
